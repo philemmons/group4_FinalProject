27adae6584c5a76054f1a6b8240944a4fb5f29ee
philemmons <pemmons@csumb.edu>
2023-11-30 21:26:24 -0800

    under construction deployed

---
+++ underConstruction.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Under Construction</title>
+    <link rel="stylesheet" href="css/uc_style.css">
+</head>
+
+<body>
+    <div id="article">
+        <h1>Our site is getting a little tune up and some love.</h1>
+        <div>
+            <p>We apologize for the inconvenience, but we're performing some maintenance while migrating from heroku's free platform. You can still contact us at <a href="mailto:philemmons.now@gmail.com">philemmons.now@gmail.com</a>. We'll be back up soon!</p>
+        </div>
+    </div>
+    <script src="index.js"></script>
+</body>
+
+</html>
