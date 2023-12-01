71ea1ff6d1034526253965fa80b55c3abbf74ae2
philemmons <pemmons@csumb.edu>
2023-12-01 00:45:01 -0800

    even more css

--- underConstruction.php
+++ underConstruction.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
+    <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Under Construction</title>
@@ -14,11 +14,10 @@
         <h1>Our site is getting a little tune up and some love.</h1>
         <div>
             <p>We apologize for the inconvenience, but we're performing some maintenance while migrating from heroku's free platform. You can still contact me at <a href="mailto:philemmons.now@gmail.com">philemmons.now@gmail.com</a>.</p>
-            <h4>In the meantime, enjoy Seychelles!</h4>
+            <h2>In the meantime, enjoy Seychelles!</h2>
             <p>Lined with dense thickets of palm trees, including six species only found here, the beaches of this Indian Ocean archipelago are impossibly scenic. On some islands in the chain, you'll be sharing the space with the Aldabra giant tortoise, a species as impressively gargantuan as its cousins in the Galápagos. On others, you might find yourself rubbing shoulders with the glitterati: The Clooneys, the Beckhams, and Prince William and Kate Middleton all honeymooned on the uber-exclusive North Island. If you don't have private island money, consider the stunning Anse Source d'Argent, which requires a small access fee to enter through a former coconut and vanilla plantation. It's reportedly among the most photographed beaches in the world, and when you see the elephant-like granite boulders strewn about the sand, you'll understand why.</p>
         </div>
     </div>
-    <script src="index.js"></script>
 </body>
 
 </html>
